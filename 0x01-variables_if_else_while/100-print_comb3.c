2cfde5d0336191623fa2d497b989a5b712044ded
Lawiezzy <aremulawal989@yahoo.com>
2023-03-19 22:54:29 -0700

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - print only smallest combinations
+  * of two-digit numbers, separated by comma
+  * followed by a new line
+  * Return: always zero (success)
+  */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || n != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
