b3cfe3cf4f85e300351e4dba0a1cc69839b9ff8e
Lawiezzy <aremulawal989@yahoo.com>
2023-03-19 23:47:32 -0700

    Create 102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - print only smallest combinations of two
+  * two-digit numbers, separated by space
+  * followed by a new line
+  * Return: always zero (success)
+  */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 100; n++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (m > n)
+			{
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
